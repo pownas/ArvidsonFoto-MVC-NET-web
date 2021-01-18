ec7f522b833a4ba58964a0606bd03099866bd372
pownas <pownas@outlook.com>
2021-01-18 12:22:08 +0100

    Tagit bort nullable på 2 fält

--- ArvidsonFoto/Models/TblImage.cs
+++ ArvidsonFoto/Models/TblImage.cs
@@ -10,10 +10,10 @@ namespace ArvidsonFoto.Models
         public int ImageId { get; set; }
         public int? ImageHuvudfamilj { get; set; }
         public int? ImageFamilj { get; set; }
-        public int? ImageArt { get; set; }
+        public int ImageArt { get; set; }
         public string ImageUrl { get; set; }
         public DateTime? ImageDate { get; set; }
         public string ImageDescription { get; set; }
-        public DateTime? ImageUpdate { get; set; }
+        public DateTime ImageUpdate { get; set; }
     }
 }
