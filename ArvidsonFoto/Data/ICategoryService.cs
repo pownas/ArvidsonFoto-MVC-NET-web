470aa8536f3974ee9379f4d0345bf13f1023e8bc
pownas <pownas@outlook.com>
2021-01-18 11:35:26 +0100

    Kommenterat bort CategoryService
    Ska skapa en databas via scaffolding istället

--- ArvidsonFoto/Data/ICategoryService.cs
+++ ArvidsonFoto/Data/ICategoryService.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ArvidsonFoto.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using ArvidsonFoto.Models;
 
-namespace ArvidsonFoto.Data
-{
-    public interface ICategoryService
-    {
-        Task<bool> SetCategoryInsert(Category category);
-        Task<int> GetCategoryLastId();
-        Task<int> GetCategoryIdByName(string categoryName);
-        Task<IEnumerable<Category>> GetAllCategoriesList();
-        Task<IEnumerable<Category>> GetCategorySubsList(int categoryID);
-    }
-}
+//namespace ArvidsonFoto.Data
+//{
+//    public interface ICategoryService
+//    {
+//        Task<bool> SetCategoryInsert(Category category);
+//        Task<int> GetCategoryLastId();
+//        Task<int> GetCategoryIdByName(string categoryName);
+//        Task<IEnumerable<Category>> GetAllCategoriesList();
+//        Task<IEnumerable<Category>> GetCategorySubsList(int categoryID);
+//    }
+//}
