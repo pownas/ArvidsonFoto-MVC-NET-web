033847e805e140262a6104f1f779fbd2a7c634ba
pownas <pownas@outlook.com>
2021-01-21 21:43:02 +0100

    Byggstart på senast-sidan

--- ArvidsonFoto/Controllers/SenastController.cs
+++ ArvidsonFoto/Controllers/SenastController.cs
@@ -3,32 +3,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArvidsonFoto.Data;
+using ArvidsonFoto.Models;
 
 namespace ArvidsonFoto.Controllers
 {
     public class SenastController : Controller
     {
-        [Route("[controller]/{sortOrder}")]
-        public IActionResult Index(string sortOrder)
+        private IImageService _imageService;
+
+        public BilderController(ArvidsonFotoDbContext context)
         {
+            _imageService = new ImageService(context);
+        }
+
+        [Route("[controller]/{sortOrder}")]
+        public IActionResult Index(string sortOrder, int? sida)
+        {
+            GalleryViewModel viewModel = new GalleryViewModel();
+            int pageSize = 48;
+
+            if (sida is null || sida < 1)
+                sida = 1;
+
+            viewModel.CurrentPage = (int)sida - 1;
+
             if (sortOrder.Equals("Per kategori"))
             {
-                ViewData["Title"] = "Senast - Per kategori";
+                ViewData["Title"] = "Per kategori";
+                viewModel.AllImagesList = _imageService.GetAllImagesList()
             }
             else if (sortOrder.Equals("Uppladdad"))
             {
-                ViewData["Title"] = "Senast - Uppladdad";
+                ViewData["Title"] = "Uppladdad";
             }
             else if (sortOrder.Equals("Fotograferad"))
             {
-                ViewData["Title"] = "Senast - Fotograferad";
+                ViewData["Title"] = "Fotograferad";
             }
             else
             {
                 return RedirectToAction("Index", new { sortOrder = "Fotograferad" });
             }
 
-            return View();
+            return View(viewModel);
         }
 
         public IActionResult Index()
