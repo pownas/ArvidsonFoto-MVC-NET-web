945c3aa0d0e591332d8048c13ec6717c54bf2fd3
pownas <pownas@outlook.com>
2021-01-26 16:57:37 +0100

    Lagt till Identity Scaffolding med ALLA sidor och en ny datamodell för inloggad användare
    Behöver bygga om och dölja flertalet av dessa sidorna...

---
+++ ArvidsonFoto/Areas/Identity/Data/ArvidsonFotoUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ArvidsonFoto.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ArvidsonFotoUser class
+    public class ArvidsonFotoUser : IdentityUser
+    {
+    }
+}
---
+++ ArvidsonFoto/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ArvidsonFoto.Areas.Identity.Data;
+using ArvidsonFoto.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ArvidsonFoto.Areas.Identity.IdentityHostingStartup))]
+namespace ArvidsonFoto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ArvidsonFotoIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ArvidsonFotoIdentityContextConnection")));
+
+                services.AddDefaultIdentity<ArvidsonFotoUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ArvidsonFotoIdentityContext>();
+            });
+        }
+    }
+}
