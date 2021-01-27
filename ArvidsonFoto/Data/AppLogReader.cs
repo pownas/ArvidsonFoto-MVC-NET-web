e77ea4188b1e15c21194740b898d55d737f2a3e7
pownas <pownas@outlook.com>
2021-01-27 03:44:43 +0100

    Skapat AppLogReader, för att läsa loggfiler med
    Skapat en AppLogReader, ska användas för att läsa logg-filerna under /logs/ , via Admin-sidan: "Visa loggboken"

---
+++ ArvidsonFoto/Data/AppLogReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArvidsonFoto.Data
+{
+    public class AppLogReader
+    {
+        string folderDataPath = @"..\..\logs\";
+
+        public List<string> ReadData(string appLogFile)
+        {
+            string inputDataPath = folderDataPath + appLogFile;
+            List<string> returnList = new List<string>();
+            using (StreamReader reader = File.OpenText(inputDataPath))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    returnList.Add(line);
+                }
+            }
+            return returnList;
+        }
+    }
+}
