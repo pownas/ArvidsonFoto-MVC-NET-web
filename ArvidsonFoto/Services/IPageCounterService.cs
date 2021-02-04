7e855bc7ea6093dc729232805197a3cb45baece9
pownas <pownas@outlook.com>
2021-02-04 21:18:47 +0100

    Gjort klart för lite mer PageCounter statistik

--- ArvidsonFoto/Services/IPageCounterService.cs
+++ ArvidsonFoto/Services/IPageCounterService.cs
@@ -1,7 +1,14 @@
-﻿namespace ArvidsonFoto.Services
+﻿using System.Collections.Generic;
+using ArvidsonFoto.Models;
+
+namespace ArvidsonFoto.Services
 {
     public interface IPageCounterService
     {
         void AddPageCount(string pageName);
+
+        List<TblPageCounter> GetMonthCount(string yearMonth);
+
+        List<TblPageCounter> GetAllGroupedByPageCount();
     }
 }
--- ArvidsonFoto/Services/PageCounterService.cs
+++ ArvidsonFoto/Services/PageCounterService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ArvidsonFoto.Data;
 using ArvidsonFoto.Models;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace ArvidsonFoto.Services
@@ -50,6 +51,40 @@ namespace ArvidsonFoto.Services
             {
                 Log.Error("Error while updating PageCounter. Error-message: " + ex.Message);
             }
+        }
+
+        /// <summary>
+        /// En funktion som hämtar månadens sidvisningar. Exempel på input: "2021-02"
+        /// </summary>
+        /// <param name="YearMonth">Tar en DateTime likt "2021-02", exempel: DateTime.Now.ToString("yyyy-MM")</param>
+        /// <returns></returns>
+        public List<TblPageCounter> GetMonthCount(string yearMonth)
+        {
+            List<TblPageCounter> listToReturn = new List<TblPageCounter>();
+            listToReturn = _entityContext.TblPageCounter
+                                         .Where(p => p.MonthViewed.Equals(yearMonth))
+                                         .OrderByDescending(p => p.PageViews)
+                                         .ToList();
+            return listToReturn;
+        }
+
+        public List<TblPageCounter> GetAllGroupedByPageCount()
+        {
+            List<TblPageCounter> listToReturn = new List<TblPageCounter>();
+            //var groupedList = _entityContext.TblPageCounter
+            //                             .GroupBy(p => new { p.PageName })
+            //                             .Select(g => new { PageName = g.Key.PageName, Visningar = g.Count() })
+            //                             //.OrderByDescending(p => p.PageViews)
+            //                             .ToList();
+
+            listToReturn = _entityContext.TblPageCounter
+                                         .FromSqlRaw("SELECT SUM(PageCounter_Views) AS PageCounter_Views, PageCounter_Name, MAX(PageCounter_LastShowDate) AS PageCounter_LastShowDate " +
+                                                     "FROM tbl_PageCounter "+
+                                                     "GROUP BY PageCounter_Name "+
+                                                     "ORDER BY PageCounter_Views DESC")
+                                         .ToList();
+
+            return listToReturn;
         }
     }
 }
