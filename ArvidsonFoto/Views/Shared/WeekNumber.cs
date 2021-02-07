4d0dfa4dd9243d533cac4be9ec145f8f6e633ad9
pownas <pownas@outlook.com>
2021-02-07 23:30:01 +0100

    Edited for Get() sv-SE

--- ArvidsonFoto/Views/Shared/WeekNumber.cs
+++ ArvidsonFoto/Views/Shared/WeekNumber.cs
@@ -5,11 +5,12 @@ namespace ArvidsonFoto.Views.Shared
 {
     public static class WeekNumber
     {
-        public static void Main()
+        public static int Get()
         {
+            //Code from: https://docs.microsoft.com/en-us/dotnet/api/system.globalization.calendar.getweekofyear?view=net-5.0
 
             // Gets the Calendar instance associated with a CultureInfo.
-            CultureInfo myCI = new CultureInfo("en-US");
+            CultureInfo myCI = new CultureInfo("sv-SE");
             Calendar myCal = myCI.Calendar;
 
             // Gets the DTFI properties required by GetWeekOfYear.
@@ -17,13 +18,15 @@ namespace ArvidsonFoto.Views.Shared
             DayOfWeek myFirstDOW = myCI.DateTimeFormat.FirstDayOfWeek;
 
             // Displays the number of the current week relative to the beginning of the year.
-            Console.WriteLine("The CalendarWeekRule used for the en-US culture is {0}.", myCWR);
-            Console.WriteLine("The FirstDayOfWeek used for the en-US culture is {0}.", myFirstDOW);
-            Console.WriteLine("Therefore, the current week is Week {0} of the current year.", myCal.GetWeekOfYear(DateTime.Now, myCWR, myFirstDOW));
+            //Console.WriteLine("The CalendarWeekRule used for the en-US culture is {0}.", myCWR);
+            //Console.WriteLine("The FirstDayOfWeek used for the en-US culture is {0}.", myFirstDOW);
+            //Console.WriteLine("Therefore, the current week is Week {0} of the current year.", myCal.GetWeekOfYear(DateTime.Now, myCWR, myFirstDOW));
 
             // Displays the total number of weeks in the current year.
             DateTime LastDay = new System.DateTime(DateTime.Now.Year, 12, 31);
-            Console.WriteLine("There are {0} weeks in the current year ({1}).", myCal.GetWeekOfYear(LastDay, myCWR, myFirstDOW), LastDay.Year);
+            //Console.WriteLine("There are {0} weeks in the current year ({1}).", myCal.GetWeekOfYear(LastDay, myCWR, myFirstDOW), LastDay.Year);
+
+            return myCal.GetWeekOfYear(LastDay, myCWR, myFirstDOW);
         }
     }
 }
