128cf97a09cb5309df2a15e40224029289e64064
pownas <pownas@outlook.com>
2021-02-09 16:54:09 +0100

    Uppdaterat så att LogReader kan nu läsa webbserver .log-filerna

---
+++ ArvidsonFoto-LogReader/Model/LogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArvidsonFoto_LogReader.Model
+{
+    public class LogPost
+    {
+        public string IPadress { get; set; }
+        public string Date { get; set; } // public DateTime Date { get; set; }
+        public string HTTPmethod { get; set; }
+        public string UrlPath { get; set; }
+        public string ErrorCode { get; set; }
+        public string TimeToLoad { get; set; }
+        public string RequestFrom { get; set; } //????
+        public string Browser { get; set; }
+    }
+}
