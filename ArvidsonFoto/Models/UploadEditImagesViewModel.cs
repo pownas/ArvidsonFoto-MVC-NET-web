1b21621f641088b90a88d92a11581c725faaf48c
Jonas A <pownas@outlook.com>
2021-02-12 11:54:51 +0100

    v3.2.1 - inläsning till main (#52)
    Commits: 
    
    * Tagit bort en duplicerad rad
    
    * SVARA GB-Lagt till en kontroll om det finns en mail eller ej
    
    Samt Svara fungerar nu och öppnar i Outlook
    Löser Issue #39
    
    * RedigeraBilder - Rättning av Art
    
    En rättning som gör att Art ska raderas om den inte länkat rätt.
    
    * Lagt till lite nofollow
    
    För att lösa: #48
    
    * Lagt till en bild hur admin ser sidan inloggad
    
    * Fix för #37
    
    #37
    
    * Fix för att lösa datumet
    
    Denna uppdateringen ska agera lösning för:
    #35
    
    * Rättning för fåglar som bara har 2 nivåer
    
    Löser felet i: #49
    
    * Byggt klart så det går att ändra bilder
    
    Löser: #29
    
    * ÅÄÖ rättning
    
    Bör lösa lite felmeddelanden i loggen likt:
    
    /Bilder/GrÃ¥spett
    /Bilder/TrÃ¤dgÃ¥rdsvÃ¤xter
    /Bilder/PrÃ¤rielÃ¶pare
    
    * Tagit bort onödig using
    
    * v3.2.1 - Flertalet buggfixar
    
    #49 - Fel vid uppladdning av vissa fåglar
    #48 - Galleriet - nofollow på länkar till Köp av bilder
    #39 - Svara på GB-inlägg, ska bara visas på de som har Mail
    #37 - Skickat Mail meddelande långt åt höger
    #35 - Fotodatum - fel vid editering och uppladdning på mobil
    #29 - Fixa så det går att ändra bilder

--- ArvidsonFoto/Models/UploadEditImagesViewModel.cs
+++ ArvidsonFoto/Models/UploadEditImagesViewModel.cs
@@ -6,15 +6,12 @@ namespace ArvidsonFoto.Models
     public class UploadEditImagesViewModel
     {
         public List<UploadImageInputModel> DisplayImagesList { get; set; }
-
         public List<TblImage> AllImagesList { get; set; }
-
-        //public UploadImageInputModel ImageInputModel { get; set; } //För att editera??
 
         public int TotalPages { get; set; }
         public int CurrentPage { get; set; }
         public string CurrentUrl { get; set; }
         public string DisplayMessage { get; set; }
-        public string UpdatedId { get; set; }
+        public int? UpdatedId { get; set; }
     }
 }
