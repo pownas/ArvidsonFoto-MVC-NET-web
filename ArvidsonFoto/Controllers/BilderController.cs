bbc6747e4c462ec92c6afd1a996d0363197e6ce5
pownas <pownas@outlook.com>
2021-02-12 11:39:07 +0100

    Tagit bort onödig using

--- ArvidsonFoto/Controllers/BilderController.cs
+++ ArvidsonFoto/Controllers/BilderController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using ArvidsonFoto;
 using ArvidsonFoto.Data;
 using ArvidsonFoto.Models;
 using ArvidsonFoto.Services;
