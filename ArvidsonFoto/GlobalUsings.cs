273ea4e814fe0f309ca44866c3868a60410f31fe
pownas <pownas@outlook.com>
2021-10-26 22:47:46 +0200

    Create GlobalUsings.cs
    Skapar en fil som innehåller de flesta usings som upprepar sig.

---
+++ ArvidsonFoto/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using ArvidsonFoto.Data;
+global using ArvidsonFoto.Models;
+global using ArvidsonFoto.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Serilog;
+
+namespace ArvidsonFoto;
