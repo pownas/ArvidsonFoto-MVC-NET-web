127e639676b2e97e0698e05c1cba6d88060955c1
Jonas A <pownas@outlook.com>
2021-11-22 19:41:50 +0100

    Update DbSeederExtension.cs

--- ArvidsonFoto/Data/DbSeederExtension.cs
+++ ArvidsonFoto/Data/DbSeederExtension.cs
@@ -144,6 +144,6 @@ public static class DbSeederExtension
             ImageUpdate = new DateTime(2021, 11, 22, 16, 21, 00)
         };
 
-        modelBuilder.Entity<TblImage>().HasData(image1, image2);
+        modelBuilder.Entity<TblImage>().HasData(image1, image2, image3, image4, image5, image6, image7, image8, image9);
     }
 }
