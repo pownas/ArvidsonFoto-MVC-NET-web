18f2ed67778a218ed0bd6d0ac64fc06e9fc05a2e
Jonas A <pownas@outlook.com>
2021-11-22 15:51:06 +0100

    Lägger till ImplicitUsings

--- ArvidsonFoto/GlobalUsings.cs
+++ ArvidsonFoto/GlobalUsings.cs
@@ -1,8 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Threading.Tasks;
-global using ArvidsonFoto.Data;
+﻿global using ArvidsonFoto.Data;
 global using ArvidsonFoto.Models;
 global using ArvidsonFoto.Services;
 global using Microsoft.AspNetCore.Mvc;
