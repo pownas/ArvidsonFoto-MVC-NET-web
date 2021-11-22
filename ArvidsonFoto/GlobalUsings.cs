4a0c42c1746d351a7290ae61db63890e6767eb80
Jonas A <pownas@outlook.com>
2021-11-22 22:14:56 +0100

    Hittat en bugg på "Senast/PerKategori" som behöver lösas

--- ArvidsonFoto/GlobalUsings.cs
+++ ArvidsonFoto/GlobalUsings.cs
@@ -3,5 +3,6 @@ global using ArvidsonFoto.Models;
 global using ArvidsonFoto.Services;
 global using Microsoft.AspNetCore.Mvc;
 global using Serilog;
+global using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ArvidsonFoto;
