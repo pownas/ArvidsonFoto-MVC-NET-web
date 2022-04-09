92d85717d94aaea3b370d728b4fc2cf9c5ca80a7
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-04-10 01:57:31 +0200

    Adding start for fb-client

---
+++ ArvidsonFoto/Data/FacebookClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace ArvidsonFoto.Data;
+
+/// <summary>
+/// Facebook Client connector to FB API. 
+/// Blogpost that explains more: https://piotrgankiewicz.com/2017/02/06/accessing-facebook-api-using-c/
+/// </summary>
+public interface IFacebookClient
+{
+    Task<T> GetAsync<T>(string accessToken, string endpoint, string args = null);
+    Task PostAsync(string accessToken, string endpoint, object data, string args = null);
+}
+
+public class FacebookClient : IFacebookClient
+{
+    private readonly HttpClient _httpClient;
+
+    public FacebookClient()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://graph.facebook.com/")
+        };
+        _httpClient.DefaultRequestHeaders
+            .Accept
+            .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<T> GetAsync<T>(string accessToken, string endpoint, string args = null)
+    {
+        var response = await _httpClient.GetAsync($"{endpoint}?access_token={accessToken}&{args}");
+        if (!response.IsSuccessStatusCode)
+            return default(T);
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<T>(result);
+    }
+
+    public async Task PostAsync(string accessToken, string endpoint, object data, string args = null)
+    {
+        var payload = GetPayload(data);
+        await _httpClient.PostAsync($"{endpoint}?access_token={accessToken}&{args}", payload);
+    }
+
+    private static StringContent GetPayload(object data)
+    {
+        var json = JsonSerializer.Serialize(data);
+
+        return new StringContent(json, Encoding.UTF8, "application/json");
+    }
+}
---
+++ ArvidsonFoto/Services/FacebookApiService.cs
@@ -0,0 +1,48 @@
+﻿namespace ArvidsonFoto.Services;
+
+public interface IFacebookService
+{
+    //Task<Account> GetAccountAsync(string accessToken);
+    Task PostOnWallAsync(string accessToken, string message);
+}
+
+/// <summary>
+/// Service to connect to Facebook API, and send API-calls
+/// Code are partly from this blogpost: https://piotrgankiewicz.com/2017/02/06/accessing-facebook-api-using-c/
+/// </summary>
+public class FacebookApiService : IFacebookService
+{
+    private readonly IFacebookClient _facebookClient;
+
+    public FacebookApiService(IFacebookClient facebookClient)
+    {
+        _facebookClient = facebookClient;
+    }
+
+    //public async Task<Account> GetAccountAsync(string accessToken)
+    //{
+    //    var result = await _facebookClient.GetAsync<dynamic>(
+    //        accessToken, "me", "fields=id,name,email,first_name,last_name,age_range,birthday,gender,locale");
+
+    //    if (result == null)
+    //    {
+    //        return new Account();
+    //    }
+
+    //    var account = new Account
+    //    {
+    //        Id = result.id,
+    //        Email = result.email,
+    //        Name = result.name,
+    //        UserName = result.username,
+    //        FirstName = result.first_name,
+    //        LastName = result.last_name,
+    //        Locale = result.locale
+    //    };
+
+    //    return account;
+    //}
+
+    public async Task PostOnWallAsync(string accessToken, string message)
+        => await _facebookClient.PostAsync(accessToken, "me/feed", new { message });
+}
