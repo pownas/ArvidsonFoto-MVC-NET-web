5f239f1b09779c199fd577cae81f28a078bc4ea9
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-04-09 23:01:24 +0200

    upgrade logreader to dotnet v6

--- ArvidsonFoto-LogReader/Model/LogPost.cs
+++ ArvidsonFoto-LogReader/Model/LogPost.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿namespace ArvidsonFoto_LogReader.Model;
 
-namespace ArvidsonFoto_LogReader.Model
+public class LogPost
 {
-    public class LogPost
-    {
-        public string IPadress { get; set; }
-        public string Date { get; set; } // public DateTime Date { get; set; }
-        public string HTTPmethod { get; set; }
-        public string UrlPath { get; set; }
-        public string ErrorCode { get; set; }
-        public string TimeToLoad { get; set; }
-        public string RequestFrom { get; set; } //????
-        public string Browser { get; set; }
-    }
+    public string IPadress { get; set; }
+    public string Date { get; set; } // public DateTime Date { get; set; }
+    public string HTTPmethod { get; set; }
+    public string UrlPath { get; set; }
+    public string ErrorCode { get; set; }
+    public string TimeToLoad { get; set; }
+    public string RequestFrom { get; set; } //????
+    public string Browser { get; set; }
 }
